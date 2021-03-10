e8b3b0fd78602996a020951f66945e4406aa3a86
TheAntiiReadyReaction <80123698+TheAntiiReadyReaction@users.noreply.github.com>
2021-03-10 09:22:36 -0500

    Create prototype.js

---
+++ prototype.js
@@ -0,0 +1,17 @@
+fetch("https://login.i-ready.com/student/v2/web/lesson_component/DI.MATH.GEO.8.1005.phx.10_71da0b35-cb71-4540-9e75-73203c6686dc_M_math/markprogress/DI.MATH.GEO.8.1005.phx.10_71da0b35-cb71-4540-9e75-73203c6686dc_M_math", {
+  "headers": {
+    "accept": "*/*",
+    "accept-language": "en-US,en;q=0.9",
+    "content-type": "application/json;charset=UTF-8",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-origin",
+    "sec-gpc": "1"
+  },
+  "referrer": "https://login.i-ready.com/student/dashboard/home",
+  "referrerPolicy": "strict-origin-when-cross-origin",
+  "body": "{\"value\":\"{\\\"finalStageArr\\\":[],\\\"curSlideInt\\\":22,\\\"slideCompletedArr\\\":[true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,true,false],\\\"allScoreDataArr\\\":[],\\\"stateStoreDataObj\\\":{}}\",\"bucket\":\"short_term_unsecured\",\"datatype\":\"json\"}",
+  "method": "POST",
+  "mode": "cors",
+  "credentials": "include"
+});
